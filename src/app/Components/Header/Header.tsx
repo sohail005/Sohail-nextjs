4fd03a22875ef20aa9cc0d0c187518773368fcef
sohail005 <sohail345patel@gmail.com>
2025-09-06 12:44:46 +0530

    Add logo image and update Header component to use imported logo

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import React from 'react'
-
+import logo from "../../../Assets/Images/logo.png";
 function Header() {
   return (
     <div className='Header p-4 flex justify-around items-center'>
-        <Image src={require('../../../Assets/Images/logo.png')} alt="Vercel Logo" width={100} height={16} />
+        <Image src={logo} alt="Vercel Logo" width={100} height={16} />
        <div className='nav'>
         <ul className='flex gap-4'>
             <li className='text-black hover:text-blue-500 cursor-pointer'>Desitnations</li>
@@ -16,8 +16,7 @@ function Header() {
        <div className='nav'>
         <ul className='flex gap-4'>
             <li className='text-black hover:text-blue-500 cursor-pointer'>Login</li>
-            <li className='text-black hover:text-blue-500 cursor-pointer'>Hotels</li>
-            <li className='text-black hover:text-blue-500 cursor-pointer'>Flights</li>
+            <button className='text-black hover:text-blue-500 cursor-pointer border border-black hover:border-blue-500 cursor-pointer rounded px-2'>Sign up</button>
           {/* https://www.figma.com/design/c9sj92UhDHPIZOQUomMtOV/Travel-Website-Landing-Page--Community-?node-id=0-1&p=f&t=KqSEvZBnqdIYgJAW-0 */}
           {/* https://vercel.com/sohail005s-projects/sohail-nextjs/4J45Tdf7hEPCfPSnGeQzxwobpsRf */}
         </ul>
